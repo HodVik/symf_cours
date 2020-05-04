74ae9f5a97638039f91d5f026b086ee341a4ccd9
HodVik <viktor.hodulian@gmail.com>
2020-05-04 18:22:47 +0300

    04.05.2020 auth completed

--- estt/src/Controller/SecurityUserController.php
+++ estt/src/Controller/SecurityUserController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\SecurityUser;
 use App\Form\SecurityUserType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityUserController extends AbstractController
 {
@@ -39,5 +41,30 @@ class SecurityUserController extends AbstractController
        return $this->render('security_user/siginup.html.twig',[
            'regForm' => $regForm->createView(),
        ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
